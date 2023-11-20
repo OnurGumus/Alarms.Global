363e70f88edcc494dfa303a28908e09ea17a36d2
Onur Gumus <onur@outlook.com.tr>
2023-11-20 16:41:57 +0000

    add mock countries with app environment
    Change-Id: I490be1420a7a6406b851ffb9b14df19ee3aff479

---
+++ src/Server/Environments.fs
@@ -0,0 +1,41 @@
+module HolidayTracker.Server.Environments
+
+open Microsoft.Extensions.Configuration
+open HolidayTracker.ServerInterfaces.Query
+open HolidayTracker.Shared.Model
+
+[<System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage>]
+type AppEnv(config: IConfiguration) =
+
+    interface IConfiguration with
+        member _.Item
+            with get (key: string) = config.[key]
+            and set key v = config.[key] <- v
+
+        member _.GetChildren() = config.GetChildren()
+        member _.GetReloadToken() = config.GetReloadToken()
+        member _.GetSection key = config.GetSection(key)
+
+    interface IQuery with
+        member _.Query<'t>(?filter, ?orderby, ?orderbydesc, ?thenby, ?thenbydesc, ?take, ?skip) =
+            let res =
+                if typeof<'t> = typeof<Country> then
+                    [
+                        "Argentina"
+                        "Brazil"
+                        "Canada"
+                        "Denmark"
+                        "France"
+                    ]|>
+                    List.map(fun name -> 
+                    {
+                        CountryId = CountryId.CreateNew() 
+                        Name = name |> ShortString.TryCreate |> forceValidate
+                    })
+                    |> List.ofSeq |> box
+
+                else
+                    failwith "Not implemented"
+
+            async { return res :?> list<'t> }
+    member _.Reset() = ()
--- src/Server/Program.fs
+++ src/Server/Program.fs
@@ -147,8 +147,7 @@ let main args =
 
     let mutable ret = 0
 
-    let appEnv = new obj ()
-
+    let appEnv = new Environments.AppEnv(config)
     try
         try
             (host appEnv args).Run()
