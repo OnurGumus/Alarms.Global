d46829f396f3d6a588065d8891b36024d527fc36
Onur Gumus <onur@outlook.com.tr>
2023-11-20 16:24:50 +0000

    add Server Interfaces
    Change-Id: I983e088e987cd06ad784d983c7b10a5ac769da89

---
+++ src/ServerInterfaces/Query.fs
@@ -0,0 +1,6 @@
+﻿module HolidayTracker.ServerInterfaces.Query
+open HolidayTracker.Shared.Model
+
+[<Interface>]
+type IQuery =
+    abstract Query<'t> : ?filter:Predicate * ?orderby:string * ?orderbydesc:string * ?thenby:string  * ?thenbydesc:string * ?take:int * ?skip:int -> list<'t> Async
