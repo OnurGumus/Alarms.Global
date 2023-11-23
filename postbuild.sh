ae3210c02e8a19a7ce0ac4e4cf0cc9585eaa1f76
Onur Gumus <onur@outlook.com.tr>
2023-11-23 15:30:26 +0000

    add automation project
    Change-Id: Iedf85022f6ed26cb6815b3445001ec2ec0b9ca14

--- postbuild.sh
+++ postbuild.sh
@@ -3,3 +3,9 @@
 dotnet tool restore
 dotnet paket install
 dotnet build
+
+cd test/Automation
+pwsh bin/Debug/net8.0/playwright.ps1 install
+pwsh bin/Debug/net8.0/playwright.ps1 install-deps
+
+cd ../..
---
+++ test/Automation/Program.fs
@@ -0,0 +1,23 @@
+﻿module HolidayTracker.Automation.Program
+
+open System.Reflection
+open TickSpec
+
+[<EntryPointAttribute>]
+let main _ =
+    try
+        try
+            do
+                let ass = Assembly.GetExecutingAssembly()
+                let definitions = StepDefinitions(ass)
+
+                [ "Subscribe" ]
+                |> Seq.iter (fun source ->
+                    let stream = ass.GetManifestResourceStream("Automation." + source + ".feature")
+                    definitions.Execute(source, stream))
+            0
+        with e ->
+            printf "%A" e
+            -1
+    finally
+        Setup.host.StopAsync().Wait()
