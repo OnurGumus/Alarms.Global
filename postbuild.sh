3e0a08bb1e46751effb004a495aa2b02c975d952
Onur Gumus <onur@outlook.com.tr>
2023-11-30 13:09:25 +0000

    fix postbuild
    Change-Id: Ia6e7c010789837f6358f5f6fcaddf16d61e08443

--- postbuild.sh
+++ postbuild.sh
@@ -3,7 +3,8 @@
 dotnet tool restore
 dotnet paket install
 dotnet build
-
+npm ci
+npm run build --workspace=src/Client
 cd test/Automation
 pwsh bin/Debug/net8.0/playwright.ps1 install
 pwsh bin/Debug/net8.0/playwright.ps1 install-deps
