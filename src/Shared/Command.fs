4370442bb0f99cae6e6a68d12117c7deaffa6e11
Onur Gumus <onur@outlook.com.tr>
2023-12-28 19:22:04 +0000

    formatting
    Change-Id: Ib0de6a874f631cd6a804d09299cb861528fbc8c0

--- src/Shared/Command.fs
+++ src/Shared/Command.fs
@@ -1,13 +1,14 @@
 module HolidayTracker.Shared.Command
+
 open HolidayTracker.Shared.Model
 open HolidayTracker.Shared.Model.Subscription
 open HolidayTracker.Shared.Model.Authentication
 
-module Authentication = 
+module Authentication =
     type IdentifyUser = UserClientId -> Async<Result<Version, string list>>
 
 module Subscription =
 
-    type Subscribe = UserIdentity option-> RegionId -> Async<Result<Version, string list>>
+    type Subscribe = UserIdentity option -> RegionId -> Async<Result<Version, string list>>
     type Unsubscribe = UserIdentity option -> RegionId -> Async<Result<Version, string list>>
     type PublishEvent = GlobalEvent -> Async<Result<Version, string list>>
