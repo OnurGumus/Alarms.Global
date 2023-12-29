09df3311bd716ff5ec59b4f891986c2606dc5105
Onur Gumus <onur@outlook.com.tr>
2023-12-29 14:54:17 +0000

    fix initial build
    Change-Id: I2c2f82948afc29acd6ee57b3ca11c584c9b0cafe

--- postbuild.sh
+++ postbuild.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 npm install -g npm@10.2.5
+npm ci
 dotnet tool restore
 dotnet paket install
 dotnet build
-npm ci
 npm run build --workspace=src/Client
+
 cd test/Automation
 pwsh bin/Debug/net8.0/playwright.ps1 install
 pwsh bin/Debug/net8.0/playwright.ps1 install-deps
